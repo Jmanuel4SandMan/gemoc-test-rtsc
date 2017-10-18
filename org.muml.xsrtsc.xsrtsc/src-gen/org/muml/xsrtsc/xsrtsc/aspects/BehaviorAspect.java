1fa111ee08544cc7b803e9d795d5d3eb4fc88327
Jonas Manuel <jmanuel@msopb.de>
2017-10-18 21:54:27 +0200

    cleanup

--- org.muml.xsrtsc.xsrtsc/src-gen/org/muml/xsrtsc/xsrtsc/aspects/BehaviorAspect.java
+++
@@ -1,9 +0,0 @@
-package org.muml.xsrtsc.xsrtsc.aspects;
-
-import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
-import org.muml.xsrtsc.xsrtsc.rtsc.Behavior;
-
-@Aspect(className = Behavior.class)
-@SuppressWarnings("all")
-public abstract class BehaviorAspect {
-}
--- org.muml.xsrtsc.xsrtsc/src-gen/org/muml/xsrtsc/xsrtsc/aspects/VertexAspectVertexAspectProperties.java
+++
@@ -1,6 +0,0 @@
-package org.muml.xsrtsc.xsrtsc.aspects;
-
-@SuppressWarnings("all")
-public class VertexAspectVertexAspectProperties {
-  public boolean active = false;
-}
